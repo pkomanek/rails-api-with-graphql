e48d89f2bc8b388055b109dd2febb23bddf3ab60
Patrik Komanek <pkomanek@redhat.com>
2021-01-07 15:19:57 +0100

    Add usersCount and postsCount into the graphql.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,7 +10,7 @@ module Types
     end
 
     def users(args)
-      res = User.all
+      res = args[:id] ? User.find(args[:id]) : User.all
       res = res.drop(args[:offset]) if args[:offset]
       res = res.take(args[:limit]) if args[:limit]
       res
@@ -25,11 +25,32 @@ module Types
       User.find(id)
     end
 
-    # /users
-    field :posts, [Types::PostType], null: false
+    # /usersCount
+    field :users_count, Integer, null: false
 
-    def posts
-      Post.all
+    def users_count
+      User.all.size
+    end
+
+    # /posts
+    field :posts, [Types::PostType], null: false do
+      argument :id, ID, required: false
+      argument :limit, Integer, required: false
+      argument :offset, Integer, required: false
+    end
+
+    def posts(args)
+      res = args[:id] ? Post.find(args[:id]) : Post.all
+      res = res.drop(args[:offset]) if args[:offset]
+      res = res.take(args[:limit]) if args[:limit]
+      res
+    end
+
+    # /postsCount
+    field :posts_count, Integer, null: false
+
+    def posts_count
+      Post.all.size
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,11 +3,22 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :name, String, null: true
-    field :posts, [Types::PostType], null: true
+    field :posts, [Types::PostType], null: true do
+      argument :id, ID, required: false
+      argument :limit, Integer, required: false
+      argument :offset, Integer, required: false
+    end
     field :posts_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   
+    def posts(args)
+      res = args[:id] ? Post.find(args[:id]) : Post.all
+      res = res.drop(args[:offset]) if args[:offset]
+      res = res.take(args[:limit]) if args[:limit]
+      res
+    end
+
     def posts_count
       object.posts.size
     end
