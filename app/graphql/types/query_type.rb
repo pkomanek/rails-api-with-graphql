fded76c359232bd0312adb161fdaacb6deb3dddd
Patrik Komanek <pkomanek@redhat.com>
2021-01-07 14:13:59 +0100

    Add limit and offset for graphql requests.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,19 @@
 module Types
   class QueryType < Types::BaseObject
     # /users
-    field :users, [Types::UserType], null: false
+    field :users, [Types::UserType], null: false do
+      argument :id, ID, required: false
+      argument :limit, Integer, required: false
+      argument :offset, Integer, required: false
+      #argument :filter
+      #argument :sort_by
+    end
 
-    def users
-      User.all
+    def users(args)
+      res = User.all
+      res = res.drop(args[:offset]) if args[:offset]
+      res = res.take(args[:limit]) if args[:limit]
+      res
     end
 
     # /user
@@ -14,6 +23,13 @@ module Types
 
     def user(id:)
       User.find(id)
+    end
+
+    # /users
+    field :posts, [Types::PostType], null: false
+
+    def posts
+      Post.all
     end
   end
 end
